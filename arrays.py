1f9e57e7eb9b90fef5a8f98cea89061a0a068c07
MuahammadArsalan <muhammadarsalanforever@gmail.com>
2026-06-29 18:11:55 +0500

    Array

--- arrays.py
+++ arrays.py
@@ -19,7 +19,7 @@ print("Numpy array multiplication ", num_arr * 2)
 
 # checking time taken numpy array is effeicient in calculation
 
-import time
+'''import time
 
 inital = time.time()
 
@@ -29,4 +29,75 @@ print("time taken py list  == ", time.time()-inital)
 inital = time.time()
 num_arr = np.arange(1000000) * 2
 
-print("time taken numpy arr == ", time.time()-inital)
+print("time taken numpy arr == ", time.time()-inital)'''
+
+
+#creating arrays  using methods
+
+
+'''zeros = np.zeros((2,3))
+print(zeros)
+
+#unit matrix
+onse = np.ones((3,3))
+print(onse)
+
+full = np.full((3,2),7)
+print(full)
+
+rand = np.random.random((2,3))
+print(rand)
+
+sequencial = np.arange(0,21,2)
+print(sequencial)
+'''
+
+
+
+#Vector, Matrix and Tensor
+
+'''vector = np.array([1, 2, 3])
+print("\n","\n","\n","Vector: ", vector)
+
+matrix = np.array([[1,2,3],[4,5,6]])
+print("\n","\n","\n","Matrix: ", matrix)
+
+tensor = np.array([[[1,2],[3,4]],[[5,6],[7,8]]])
+
+print("Tensor: ", tensor)
+'''
+
+#Array properties
+
+'''arr = np.array([[1,2,3],[4,5,6]])
+print("Shape", arr.shape )
+print("dimension",arr.ndim)
+print("size",arr.size)
+print("data type",arr.dtype)
+'''
+
+
+#Array Reshaping
+
+'''arr = np.arange(12)
+print(arr)
+
+reshape = arr.reshape((4,3))
+print("reshape",reshape)'''
+
+#differnce b/w ravel and flatten is that ravel returns original array which affect the main array when changes in ravel becomes while flatten()
+#Hamesha NEW array (copy) banata hai original array pe koi effect nahi hotaSafe operation hai
+
+'''flatten = reshape.flatten()
+print("flatten",flatten)
+
+raveled = reshape.ravel()
+print("raveled",raveled)
+
+transpose = reshape.T
+print("Transpose",transpose)
+'''
+
+
+
+
